d1b73efffe454062bf7095bae14c34883cd9d5bf
Nicromano <ljose297@gmail.com>
2019-02-20 15:03:18 -0500

    elimina if innecesario

--- php/inicio.php
+++ php/inicio.php
@@ -14,46 +14,23 @@ if(!isset($usuario, $pass)){
     
 }
 
-$sql = "SELECT * FROM personal WHERE usuario = '".$usuario."' AND contrasenia = '".$pass."'";
-if(!$sentencia = $base->query($sql)){
+$sql = "SELECT * FROM personal WHERE usuario = :user AND contrasenia = :contra";
+if(!$sentencia = $base->prepare($sql)){
     echo "Error: ". $base->error;
 }
+$sentencia->bindValue(":user", $usuario, PDO::PARAM_STR);
+$sentencia->bindValue(":contra",$pass, PDO::PARAM_STR);
+$sentencia->execute();
 
-//$sentencia->bindParam(":user", $usuario, PDO::PARAM_STR);
-//$sentencia->bindParam(":contra", $pass, PDO::PARAM_STR);
-//echo $sql;
-
-//$sentencia->execute();
-if($sentencia){
-   //No hay error
-   $fila = $sentencia->num_rows;
-   if($fila != 0){
-       echo "Usuario registrado ";
-    session_start();
-    $_SESSION['usuario'] = $fila['usuario'];
-    $_SESSION['codigo'] = $fila['id'];
-    $_SESSION['cargo'] = $fila['cargo'];
-    //header('Location: ../sistema.html');
-   }
+//No hay error
+$fila = $sentencia->num_rows;
+if($fila != 0){
+    echo "Usuario registrado ";
+session_start();
+$_SESSION['usuario'] = $fila['usuario'];
+$_SESSION['codigo'] = $fila['id'];
+$_SESSION['cargo'] = $fila['cargo'];
+header('Location: ../sistema.html');
 }
-
-
-/*while ($fila = $sentencia->fetch_assoc()) {
-        echo $fila['usuario'];
-        echo "-----";
-        echo $fila['contrasenia'];
-    }
-*/
-
-/*echo "<br> ".$fila['usuario']."---- ".$fila['codigo'];
-
-if($fila['usuario']== $usuario && $fila['contraseña']== $pass){
-    
-}
-else{
-    //header('Location: ../index.html');
-    echo"Usuario sin registrar";
-}*/
-
 
 ?>
